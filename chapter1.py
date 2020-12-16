bd9df8396411c7bfa4af29a93d079d8bfec0fb4b
Paul Murff <25558342+murffious@users.noreply.github.com>
2020-12-16 08:56:31 -0700

    1.3

--- chapter1.py
+++ chapter1.py
@@ -19,7 +19,24 @@ uname, *fields, homedir, sh = line.split(':')
 # PATTERN NOTE: 3. used for createing throw away variables with unwanted items in the unpack
 # PATTERN NOTE: 4. ex: clever for list-processing liek maybe split into a head, tail, then recursively iterate 
 
-#1.3 - Unpack with a * takes those items into a list
+#1.3 - Keeping the last N items during iteration or some processing 
+from collections import deque
+def search(lines, pattern, history=5):
+    # deque(maxlen=history) creates a fixed size queue - why does this remind me of caching and memoize
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+           yield line, previous_lines
+        previous_lines.append(line)   
+# Example use on a file 
+if __name__ == '__main__':
+      with open('somefile.txt') as f:
+          for line, prevlines in search(f, 'python', 5):
+              for pline in prevlines:
+                  print(pline, end='')
+              print(line, end='')
+              print('-'*20)    
+# extended note: using generators with yield for searching and decoupling the process of searching from the code that uses the results
 
 #1.4 
 
