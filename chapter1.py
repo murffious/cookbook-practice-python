f8b51b146aa41d9ff82d04d1e1ebe6802b316355
Paul Murff <25558342+murffious@users.noreply.github.com>
2020-12-18 16:29:23 -0700

    Update chapter1.py

--- chapter1.py
+++ chapter1.py
@@ -68,4 +68,26 @@ heapq.heapify(heap)
 heap #[-4, 2, 1, 23, 7, 2, 18, 23, 42, 37, 8] can use heapq.heappop() to get next smallest on the first item
 
 
+# 1.5 Implementing a Priority Queue - sorts items by a given priority
+import heapq
+class PriorityQueue:
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
 
+class Item:
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return 'Item({!r})'.format(self.name)
+
+q = PriorityQueue()
+q.push(Item('foo'), 1)
+q.push(Item('bar'), 5)
+q.push(Item('spam'), 4)
+q.push(Item('grok'), 1)
