ac470cdd33db03a174c36b3893bc3bba7ca8a31f
Paul Murff <25558342+murffious@users.noreply.github.com>
2020-12-26 15:21:28 -0700

    also adding code for the clean python book

--- CleanPython.py
+++ CleanPython.py
@@ -1,4 +1,11 @@
 # Goal to write great Python code meaning clean quality and efficient 
 # Maintain best practice industry standards 
+# [https://www.oreilly.com/library/view/clean-python-elegant/9781484248782/]
 
 
+
+# Jargon Zen of Python or Pythonic Code - reading PEP8 docs can help know WHY  -- https://www.python.org/dev/peps/pep-0008/ or https://www.python.org/dev/peps/pep-0020/
+
+#1. Naming - lower case with underscores in between words - for variables and functions
+#_variable_name = private variable( by convention) OR __variable (two underscores) to prevetn name mangling
+#zip unzip function built in
